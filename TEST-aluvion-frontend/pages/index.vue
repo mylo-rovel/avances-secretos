f4e8a5123ce48ea42841a222d1bafc4ef783ccd8
cecirojas <cecilia.rojas@alumnos.ucn.cl>
2022-05-26 19:20:12 -0400

    idea login

--- TEST-aluvion-frontend/pages/index.vue
+++ TEST-aluvion-frontend/pages/index.vue
@@ -121,7 +121,6 @@ export default Vue.extend({
   grid-template-columns: 27% auto;
 }
 
-
 .img-left-side {
   overflow: hidden;
   border-right: 1px solid black;;
@@ -211,4 +210,107 @@ export default Vue.extend({
   font-size: 1.2rem;
   font-weight: 400;
 }
+
+/* Bordered form */
+
+form {
+    border: 3px solid #f1f1f1;
+}
+
+
+/* Full-width inputs */
+
+input[type=text],
+input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+
+/* Set a style for all buttons */
+
+button {
+    background-color: #04AA6D;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+
+/* Add a hover effect for buttons */
+
+button:hover {
+    opacity: 0.8;
+}
+
+
+/* Extra style for the cancel button (red) */
+
+.cancelbtn {
+    width: auto;
+    padding: 10px 18px;
+    background-color: #f44336;
+}
+
+
+/* Center the avatar image inside this container */
+
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+}
+
+
+/* Avatar image */
+
+img.avatar {
+    width: 40%;
+    border-radius: 50%;
+}
+
+
+/* Add padding to containers */
+
+.container {
+    padding: 16px;
+}
+
+
+/* The "Forgot password" text */
+
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+
+/* Change styles for span and cancel button on extra small screens */
+
+@media screen and (max-width: 300px) {
+    span.psw {
+        display: block;
+        float: none;
+    }
+    .cancelbtn {
+        width: 100%;
+    }
+}
+body {
+    background: #007bff;
+    background: linear-gradient(to right, #0062E6, #33AEFF);
+    
+  }
+  
+  .btn-login {
+    font-size: 0.9rem;
+    letter-spacing: 0.05rem;
+    padding: 0.75rem 1rem;
+  }
 </style>
