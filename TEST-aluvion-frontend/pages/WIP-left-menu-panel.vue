404c0e90030ebbd89765bcc09a9beba89f38ed44
Emilio Rojas Veliz <rovel.code@outlook.com>
2022-06-08 23:05:02 -0400

    finishing desktop version of side menu

--- TEST-aluvion-frontend/pages/WIP-left-menu-panel.vue
+++ TEST-aluvion-frontend/pages/WIP-left-menu-panel.vue
@@ -9,17 +9,23 @@ export default Vue.extend({
     data() {
         return {
             elementsObj : {
-                "Inicio": {open:false},
-                "Perfil": false,
-                "Secuencia": false,
-                "Simulacion": false,
-                "Contacto": false
+                // Inicio: {esDesplegable:false},
+                // Perfil: {esDesplegable:false},
+                // Simulacion: {esDesplegable:false},
+                // Contacto: {esDesplegable:false},
+                Inicio: false,
+                Perfil:  false,
+                Simulacion:  false,
+                Contacto: false,
             }
         }
     },
     methods: {
-        abrirModal(elemento) => () {
-            console.log(elemento);
+        abrirMenu(eventObj){
+            const idRouteTitle = eventObj.currentTarget.id.toString().split("_key")[0];
+            console.log(idRouteTitle)
+            // this.elementsObj[idRouteTitle].esDesplegable = !this.elementsObj[idRouteTitle].esDesplegable;
+            this.elementsObj[idRouteTitle] = !this.elementsObj[idRouteTitle];
         }
     }
 })
@@ -27,10 +33,50 @@ export default Vue.extend({
 
 <template>
 <section class="left-menu-panel">
-    <article class="menu-routes-container">
-        <p :id="element" v-for="element in Object.entries(elementsObj)" :click=""> 
-            <span class="menu-routes-element"> {{element[0]}} </span>
+    <article class="menu-routes-row-container">
+        <!-- <p v-for="element in Object.entries(elementsObj)" :key="`${element[0]}_key`"> 
+            <span :id="`${element[0]}_key`" @click="(e) => abrirMenu(e)"> 
+                <span class="menu-routes-element">
+                    {{element[0]}}
+                </span>
+                <ul v-if="element[1].esDesplegable && element[0]==='Simulación'" class="submenu-routes-menu">
+                    <p ><NuxtLink to="/operador/">Registrar simulacion</NuxtLink></p>
+                    <p ><NuxtLink to="/operador/">Iniciar simulacion</NuxtLink></p>
+                    <p ><NuxtLink to="/operador/">Ver simulacion</NuxtLink></p>
+                    <p ><NuxtLink to="/operador/">Historial de simulaciones</NuxtLink></p>
+                </ul>
+            </span>
+        </p> -->
+        <p  :id="`Inicio_key`" @click="(e) => {}">
+            <span class="menu-routes-element underlined-element">
+                <NuxtLink to="/operador/">Inicio</NuxtLink>
+            </span>
         </p>
+
+        <p :id="`Perfil_key`" @click="(e) => {}">
+            <span class="menu-routes-element">
+                <NuxtLink to="/operador/">Perfil</NuxtLink>
+            </span>
+        </p>
+
+        <p :id="`Simulacion_key`" @click="(e) => abrirMenu(e)">
+            <span class="menu-routes-element">
+                Simulación
+            </span>
+            <ul v-if="elementsObj['Simulacion']" class="submenu-routes-menu">
+                <p ><NuxtLink to="/operador/">Registrar simulacion</NuxtLink></p>
+                <p ><NuxtLink to="/operador/">Iniciar simulacion</NuxtLink></p>
+                <p ><NuxtLink to="/operador/">Ver simulacion</NuxtLink></p>
+                <p ><NuxtLink to="/operador/">Historial de simulaciones</NuxtLink></p>
+            </ul>
+        </p>
+
+        <p :id="`Contacto_key`" @click="(e) => {}">
+            <span class="menu-routes-element">
+                <NuxtLink to="/operador/">Contacto</NuxtLink>
+            </span>
+        </p>
+
     </article>
 </section>
 </template>
@@ -41,39 +87,64 @@ export default Vue.extend({
 * {
   margin:0;
   font-family: 'Poppins', sans-serif;
+  text-decoration: none;
+}
+
+p, a {
+    color:rgb(65, 65, 65);
+    text-decoration: none;
 }
 
 .left-menu-panel {
     background-color: #FFFFFF;
     height: 100vh;
-    width: fit-content;
-    padding: 7rem;
+    width: min-content;
+    min-width: 19rem;
     border: 1px solid black;
     border-radius: 3px;
     border-left: 0;
     border-top:0;
     border-bottom:0;
-    display: grid;
-    place-items: center;
 }
 
-.menu-routes-container {
-    height:fit-content;
-    width: fit-content;
+.menu-routes-row-container {
+    height: fit-content;
+    padding-top: 69%;
+    width: 100%;
+    padding-left: 5.5rem;
 }
 
-.menu-routes-container p{
-    padding-bottom: 1.25rem;
+.menu-routes-row-container p{
+    padding-bottom: 1rem;
 }
 
-.menu-routes-container p:first-child span {
+.underlined-element {
     width:fit-content;
     text-decoration: underline;
-    color: rgb(100, 100, 100);
 }
 
-.menu-routes-element:hover {
+.menu-routes-element:hover,
+.submenu-routes-menu p {
+    transition: linear 0.1s;
     color: blue;
+}
+
+
+.submenu-routes-menu p {
+    --li-padding-bottom: 0.5rem;
+    margin:0;
+    padding: 0;
+    width: 80%;
+    list-style: none;
+    padding-bottom: var(--li-padding-bottom);
+}
+
+.submenu-routes-menu p:last-child {
+    padding:0;
+}
+
+.submenu-routes-menu p a {
+    text-decoration: none;
 }
 
 </style>
