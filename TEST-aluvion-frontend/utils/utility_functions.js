1822f6b173757556733291ee0ba978c9b5bdf402
Emilio Rojas Veliz <rovel.code@outlook.com>
2022-07-06 15:42:00 -0400

    feat(lista-simulacion): Se agrega una funcion que facilita el uso de las listas desplegables en iniciar-simulacion

--- TEST-aluvion-frontend/utils/utility_functions.js
+++ TEST-aluvion-frontend/utils/utility_functions.js
@@ -1,3 +1,12 @@
-export const makeRequests = function() {
-    
-};
+export const setListasDesplegables = (listaRawSimulaciones) => {
+	const listaSimulacionesCrudas = listaRawSimulaciones["simulacionAcotada_"]
+	return listaSimulacionesCrudas.reduce( (acc, item) => {
+		nombreEquipo = item['nombreEquipo_'];
+		nombreSimulacion = item['nombre_'];
+		if (!(nombreEquipo in acc)) {
+			acc = {...acc, [nombreEquipo]: []};
+		}
+		acc[nombreEquipo].push(nombreSimulacion);
+		return acc;
+	}, {})
+}
