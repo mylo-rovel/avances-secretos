9cfc649ceea7a8f197a47051766a1754421e7857
Emilio Rojas Veliz <rovel.code@outlook.com>
2022-07-12 13:06:11 -0400

    fix(carpeta pythonBoard): Se recuperan los archivos para que corra el servidor

--- sataBoardServicerLINUX/src/coreBoardCommuService_pb2_grpc.py
+++ sataBoardServicerLINUX/src/coreBoardCommuService_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import domain_pb2 as domain__pb2
+import coreBoardCommuService_pb2 as coreBoardCommuService__pb2
 
 
 class CoreBoardCommuServiceStub(object):
@@ -18,23 +18,23 @@ class CoreBoardCommuServiceStub(object):
         """
         self.startSimulacion = channel.unary_unary(
                 '/CoreBoardCommuService/startSimulacion',
-                request_serializer=domain__pb2.SimulacionBoardReq.SerializeToString,
-                response_deserializer=domain__pb2.MensajeReply.FromString,
+                request_serializer=coreBoardCommuService__pb2.SimulacionBoardReq.SerializeToString,
+                response_deserializer=coreBoardCommuService__pb2.MensajeReply.FromString,
                 )
         self.getSimulacionActual = channel.unary_unary(
                 '/CoreBoardCommuService/getSimulacionActual',
-                request_serializer=domain__pb2.EmptyReq.SerializeToString,
-                response_deserializer=domain__pb2.SimulacionReply.FromString,
+                request_serializer=coreBoardCommuService__pb2.EmptyReq.SerializeToString,
+                response_deserializer=coreBoardCommuService__pb2.SimulacionReply.FromString,
                 )
         self.getLecturasSensores = channel.unary_stream(
                 '/CoreBoardCommuService/getLecturasSensores',
-                request_serializer=domain__pb2.EmptyReq.SerializeToString,
-                response_deserializer=domain__pb2.LecturaSensoresReply.FromString,
+                request_serializer=coreBoardCommuService__pb2.EmptyReq.SerializeToString,
+                response_deserializer=coreBoardCommuService__pb2.LecturaSensoresReply.FromString,
                 )
         self.sendMensajeEncendido = channel.unary_unary(
                 '/CoreBoardCommuService/sendMensajeEncendido',
-                request_serializer=domain__pb2.SaludoBoardReq.SerializeToString,
-                response_deserializer=domain__pb2.SaludoBoardReply.FromString,
+                request_serializer=coreBoardCommuService__pb2.SaludoBoardReq.SerializeToString,
+                response_deserializer=coreBoardCommuService__pb2.SaludoBoardReply.FromString,
                 )
 
 
@@ -76,23 +76,23 @@ def add_CoreBoardCommuServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
             'startSimulacion': grpc.unary_unary_rpc_method_handler(
                     servicer.startSimulacion,
-                    request_deserializer=domain__pb2.SimulacionBoardReq.FromString,
-                    response_serializer=domain__pb2.MensajeReply.SerializeToString,
+                    request_deserializer=coreBoardCommuService__pb2.SimulacionBoardReq.FromString,
+                    response_serializer=coreBoardCommuService__pb2.MensajeReply.SerializeToString,
             ),
             'getSimulacionActual': grpc.unary_unary_rpc_method_handler(
                     servicer.getSimulacionActual,
-                    request_deserializer=domain__pb2.EmptyReq.FromString,
-                    response_serializer=domain__pb2.SimulacionReply.SerializeToString,
+                    request_deserializer=coreBoardCommuService__pb2.EmptyReq.FromString,
+                    response_serializer=coreBoardCommuService__pb2.SimulacionReply.SerializeToString,
             ),
             'getLecturasSensores': grpc.unary_stream_rpc_method_handler(
                     servicer.getLecturasSensores,
-                    request_deserializer=domain__pb2.EmptyReq.FromString,
-                    response_serializer=domain__pb2.LecturaSensoresReply.SerializeToString,
+                    request_deserializer=coreBoardCommuService__pb2.EmptyReq.FromString,
+                    response_serializer=coreBoardCommuService__pb2.LecturaSensoresReply.SerializeToString,
             ),
             'sendMensajeEncendido': grpc.unary_unary_rpc_method_handler(
                     servicer.sendMensajeEncendido,
-                    request_deserializer=domain__pb2.SaludoBoardReq.FromString,
-                    response_serializer=domain__pb2.SaludoBoardReply.SerializeToString,
+                    request_deserializer=coreBoardCommuService__pb2.SaludoBoardReq.FromString,
+                    response_serializer=coreBoardCommuService__pb2.SaludoBoardReply.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -118,8 +118,8 @@ class CoreBoardCommuService(object):
             timeout=None,
             metadata=None):
         return grpc.experimental.unary_unary(request, target, '/CoreBoardCommuService/startSimulacion',
-            domain__pb2.SimulacionBoardReq.SerializeToString,
-            domain__pb2.MensajeReply.FromString,
+            coreBoardCommuService__pb2.SimulacionBoardReq.SerializeToString,
+            coreBoardCommuService__pb2.MensajeReply.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
@@ -135,8 +135,8 @@ class CoreBoardCommuService(object):
             timeout=None,
             metadata=None):
         return grpc.experimental.unary_unary(request, target, '/CoreBoardCommuService/getSimulacionActual',
-            domain__pb2.EmptyReq.SerializeToString,
-            domain__pb2.SimulacionReply.FromString,
+            coreBoardCommuService__pb2.EmptyReq.SerializeToString,
+            coreBoardCommuService__pb2.SimulacionReply.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
@@ -152,8 +152,8 @@ class CoreBoardCommuService(object):
             timeout=None,
             metadata=None):
         return grpc.experimental.unary_stream(request, target, '/CoreBoardCommuService/getLecturasSensores',
-            domain__pb2.EmptyReq.SerializeToString,
-            domain__pb2.LecturaSensoresReply.FromString,
+            coreBoardCommuService__pb2.EmptyReq.SerializeToString,
+            coreBoardCommuService__pb2.LecturaSensoresReply.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
@@ -169,7 +169,7 @@ class CoreBoardCommuService(object):
             timeout=None,
             metadata=None):
         return grpc.experimental.unary_unary(request, target, '/CoreBoardCommuService/sendMensajeEncendido',
-            domain__pb2.SaludoBoardReq.SerializeToString,
-            domain__pb2.SaludoBoardReply.FromString,
+            coreBoardCommuService__pb2.SaludoBoardReq.SerializeToString,
+            coreBoardCommuService__pb2.SaludoBoardReply.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
