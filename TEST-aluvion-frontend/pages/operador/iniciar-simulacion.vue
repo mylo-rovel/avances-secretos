7342f8f27a5ef09c82a1516374918839d60513e8
cecirojas <cecilia.rojas@alumnos.ucn.cl>
2022-07-12 22:08:11 -0400

    feat(iniciar-simulacion.vue): Se inserta en el select el id de la simulacion.

--- TEST-aluvion-frontend/pages/operador/iniciar-simulacion.vue
+++ TEST-aluvion-frontend/pages/operador/iniciar-simulacion.vue
@@ -2,6 +2,7 @@
 
     import Vue from 'vue'
     import PageHeader from '~/components/PageHeader.vue'
+    import {mapState} from "vuex";
     import CancelButtom from '~/components/CancelButtom.vue'
     import SubmitButton from '~/components/SubmitButton.vue'
     import NavbarPag from '~/components/NavbarPag.vue'
@@ -17,14 +18,18 @@
                 "tituloPag": "Sistema de Alerta Temprana Aluvional",
                 "submitbutton": "guardar",
                 simulacionesDisponibles: [],
+                listaIdsDisponibles:[],
+
                 equiposDisponibles:[],
                 equipoSeleccionado: "",
-                simulacionSeleccionada:"",
-                apiURL: "http://localhost:8081/api",
+                idSimulacionSeleccionada:0,
 
-                simulaciones: []
+                // simulaciones: [],
+                dictEquiposSimulaciones:{}
             };
         },
+        computed: mapState(["urlApi"]) ,
+        
         async mounted(){
             const JWTtoken = window.localStorage.getItem("token");
             const post_config = { 
@@ -34,63 +39,34 @@
             const url_to_fetch = `http://localhost:8081/api/simulaciones/`;
             const rawdata = await fetch(url_to_fetch, post_config).catch(err => err);
             const listaSimulacionesCrudas = await rawdata.json();
-            this.simulaciones = setListasDesplegables(listaSimulacionesCrudas);
+
             this.equiposDisponibles = Object.keys(setListasDesplegables(listaSimulacionesCrudas));
-            console.log(this.equiposDisponibles);
+            this.dictEquiposSimulaciones = {...setListasDesplegables(listaSimulacionesCrudas)};
+
+            //this.simulacionesDisponibles = setListasDesplegables(listaSimulacionesCrudas);
+            //this.equiposDisponibles = Object.keys(setListasDesplegables(listaSimulacionesCrudas));
+            //console.log(this.equiposDisponibles);
+            //console.log(listaSimulacionesCrudas);
+            //console.log(setListasDesplegables(listaSimulacionesCrudas)[this.equiposDisponibles]['listaIds']);
+            //this.listaIdsDisponibles = setListasDesplegables(listaSimulacionesCrudas)[this.equiposDisponibles]['listaIds'];
+            
+            //console.log(this.listaIdsDisponibles);
            // console.log(Object.keys(setListasDesplegables(listaSimulacionesCrudas)));
-            this.simulacionesDisponibles = setListasDesplegables(listaSimulacionesCrudas)[this.equiposDisponibles];
-            console.log(this.simulacionesDisponibles);
-            console.log();
+            // this.simulacionesDisponibles = setListasDesplegables(listaSimulacionesCrudas)[this.equiposDisponibles];
+            // console.log(this.simulacionesDisponibles);
+            // console.log();
         },
 
+        
+
         methods: {
-            async getSimulaciones ({$axios}){//enviar peticion
-                console.clear();
-                const serverPath = `${this.apiURL}/simulaciones`;
-                const serverResponse = await $axios.$get(serverPath).catch(err => err);
-                if (serverResponse instanceof Error) {
-                    alert("ERROR. rayos :(", serverResponse)
-                    return false;
-                }
-                console.log(serverResponse);
-                //this.simulaciones = serverResponse["simulacionAcotada_"];
-                return true;
-            },
-
-            //Metodo que llena las opciones del select con los equipos
-            // getEquipos: function(simulaciones) {
-            //     let selectIdEquipo = document.getElementById('select_equipoSimulacion');
-            //     for (let i = 0; i < simulaciones.length; i++) {
-            //         let opcionEquipo = document.createElement('option');
-            //         opcionEquipo.text = simulaciones[i].nombreEquipo_;
-            //         selectIdEquipo.add(opcionEquipo);
-            //     }
-            //     console.log();
-            //     return selectIdEquipo;
-            // },
-
-            //Metodo que llena las opciones del select con los id de simulaciones del equipo seleccionado
-            getSimulacionesEquipo: function(){
-                var equipoActual = document.getElementById('select_equipoSimulacion').selectedIndex;
-                for(let i = 0; i < simulaciones.length; i++ ){
-                    document.getElementById('select_idSimulacion').value = simulaciones[equipoActual].id_;
-                }
-                console.log();
-            },
-
-            //Metodo que muestra el nombre de la simulacion dada la seleccion de un id 
-            displayNombreSimulacion: function() { 
-                var simulacionActual = document.getElementById('select_idSimulacion').selectedIndex;
-                document.getElementById('nombreSimulacion').value = this.simulaciones[simulacionActual].nombre_;
-                console.log(simulacionActual);
-            },
-
             //Metodo que guarda el equipo seleccionada
             saveEquipo: function(equipoSeleccionado){
-                equipoSeleccionado = document.getElementById('select_equipoSimulacion');
-                let opcion = equipoSeleccionado.selectedOptions[0].value;
-                console.log(opcion);
-                return opcion;
+                let opcionSeleccionada = document.getElementById('select_equipoSimulacion').selectedOptions[0].value;
+                this.simulacionesDisponibles = this.dictEquiposSimulaciones[opcionSeleccionada]["listaSimulaciones"];
+                this.listaIdsDisponibles = this.dictEquiposSimulaciones[opcionSeleccionada]["listaIds"];
+                return opcionSeleccionada;
+                console.log(opcionSeleccionada);
             },
 
             //Metodo que guarda la simulacion seleccionada
@@ -101,19 +77,24 @@
                 return opcion;
             },
 
-            //Metodo que enviar solicitud de iniciar simulacion
-            async sendSolicitudSimulacion(){
-                let info = JSON.stringify({"equipo":equipoSeleccionado, "simulacion":simulacionSeleccionada});
+            
+            
 
-                // document.getElementById("form_iniciarSimulacion").submit();
+//Metodo que enviar solicitud de iniciar simulacion SE TIENE QUE ENVIAR LA ID DEL EQUIPO Y SIMULACION , NO NOMBRES
+            async sendSolicitudSimulacion(){
+                let solicitud = JSON.stringify({"nombreEquipo":this.equipoSeleccionado, "id":this.idSimulacionSeleccionada});
+                console.log(solicitud);
+
+                const tokenUsuario = window.localStorage.getItem("token");
                 const POST_config = {
                     'method': 'POST',
-                    'body': info,
-                    'authorization': 'sdzfdnfdsf'
+                    'body': solicitud,
+                    'authorization': tokenUsuario
                 };
-                const rawResponse = await fetch('https://jsonplaceholder.typicode.com/', POST_config);
-                console.log(rawResponse);
+                //const respuesta = await fetch(`${this.urlApi}/simulaciones/`, POST_config);
+                //console.log(respuesta);
             }
+
 
         }
     })
@@ -130,8 +111,6 @@
                 <div>
                     <div class="row my-4">
                         <h3>Iniciar Simulación</h3>
-                        <!--<button @click="sendSolicitudSimulacion()">aaa</button>
-                        <button @click="simus(simulaciones)">aaajjj</button>-->
                     </div>
                     <div class="row">
                         <form id="form_iniciarSimulacion" method="post">
@@ -143,20 +122,18 @@
                                     </select>
                                 </div>
                             </div>
+                           
                             <div class="my-4 form-group row">
                                 <label for="simulacion" class="col-sm-4 col-form-label" >Seleccione una simulación</label>
-                                <div  v-if= "" class="col-sm-6">
-                                    <select id="select_idSimulacion" v-model="simulacionSeleccionada" class="form-select" @change= "saveSimulacion()" aria-label="Simulacion" required>
-                                         <option v-for="i in simulaciones" :value="i">{{i}}</option>
+                                <div  class="col-sm-6">
+                                    <select id="select_idSimulacion" v-model="idSimulacionSeleccionada" class="form-select" aria-label="Simulacion" required>
+                                         <option v-for="(simulacion, indiceFila) in simulacionesDisponibles" :value="listaIdsDisponibles[indiceFila]">
+                                            {{`${listaIdsDisponibles[indiceFila]} - ${simulacion}`}}
+                                        </option> 
                                     </select>
                                 </div>
                             </div>
-                            <!--<div class="my-4 form-group row">
-                                <label for="simulacion" class="col-sm-4 col-form-label" >Nombre simulación</label>
-                                <div class="col-sm-6">
-                                    <input id="nombreSimulacion" name="nombre_simulacion" class="form-control" aria-label="Simulacion" required>
-                                </div>
-                            </div>-->
+
                             <div class = "row my-4 ">
                                 <div class= "col-12 contenido-botones my-4">
                                     <NuxtLink to="/operador/menu-operador"><CancelButtom :cancelbutton = "cancelbutton"/></NuxtLink>
@@ -166,6 +143,7 @@
                                 </div>
                             </div>
                         </form>
+                        <button @click="sendSolicitudSimulacion()">CONSOLE.LOG HERE CHECK ME OUT</button>
                     </div>
                 </div>
             </div>
